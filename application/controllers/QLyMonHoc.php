7ff2fd131ffdbd3bd4579f8eb04acbd82d8b1356
NguyenBaDat99 <55686492+NguyenBaDat99@users.noreply.github.com>
2020-04-27 22:52:40 +0700

    Tạo trang môn học

--- application/controllers/QLyMonHoc.php
+++ application/controllers/QLyMonHoc.php
@@ -6,14 +6,24 @@ class QLyMonHoc extends CI_Controller {
 	function __construct()
     {
         parent::__construct();
-        // $this->load->Model("_Model");
+        $this->load->Model("m_MonHoc");
         $this->load->library('session');
+        $this->load->helper('url');
+        $this->load->helper(array('url', 'form'));
+        
     }
 
 	public function index()
 	{
-        $data['content'] = 'home/MonHoc/v_QLyMonHoc';
-		$this->load->view('home/index', $data);
+        // $data['content'] = 'home/MonHoc/v_QLyMonHoc';
+        // $this->load->view('home/index', $data);
+        
+        $this->load->view('home/header');
+
+        $dsMonHoc['dsMonHoc'] = $this->m_MonHoc->ds_monhoc();
+        $view['content'] = $this->load->view('home/MonHoc/v_QLyMonHoc', $dsMonHoc);
+
+        $this->load->view('home/footer');
     }
 }
 	
---
+++ application/models/m_MonHoc.php
@@ -0,0 +1,68 @@
+<?php
+class m_MonHoc extends CI_Model{
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+        $this->table = 'mon_hoc';
+    }
+
+    public function ds_monhoc()    
+    {
+        $query=$this->db->get("mon_hoc");
+        return $query->result_array();
+    }
+
+    public function xoa_monhoc($maMonHoc)
+    {
+        $this->db->query("delete from mon_hoc where MaMonHoc='".$maMonHoc."'");
+    }
+
+    // public function them_nguoidung($tenNguoiDung, $matKhau, $loaiNguoiDung)
+    // {
+    //     $maNguoiDung = 1;
+    //     $ds_nguoidung = $this->ds_nguoidung();
+    //     foreach($ds_nguoidung as $item)
+    //     {
+    //         if($maNguoiDung < $item['MaNguoiDung'])
+    //         {
+    //             break;
+    //         }
+    //         $maNguoiDung++;
+    //     }
+    //     $this->db->query("insert into nguoi_dung values("
+    //     .$maNguoiDung.",'".$tenNguoiDung."', '".$matKhau."', '".$loaiNguoiDung."')");
+    //     //$this->db->query("insert into nguoi_dung values(5,'".$tenNguoiDung."', '".$matKhau."')");
+    // }
+
+    // public function tim_nguoidung($thongTin)
+    // {
+    //     if(is_numeric($thongTin))
+    //     {
+    //         $query=$this->db->query("select * from nguoi_dung where MaNguoiDung=".$thongTin);
+    //         return $query->result_array();
+    //     }
+    //     else
+    //     {
+    //         $query=$this->db->query("call tim_nguoidung('".$thongTin."');");
+    //         return $query->result_array();
+    //     }
+    // }
+    // public function tim_nguoidung_ten($tenNguoiDung)
+    // {
+    //     $query=$this->db->query("select * from nguoi_dung where TenNguoiDung='".$tenNguoiDung."'");
+    //     return $query->row_array();
+    // }
+
+    // public function tim_nguoidung_id($maNguoiDung)
+    // {
+    //     $query=$this->db->query("select * from nguoi_dung where MaNguoiDung=".$maNguoiDung."");
+    //     return $query->row_array();
+    // }
+
+    // public function sua_nguoidung($maNguoiDung, $tenNguoiDung, $matKhau, $loaiNguoiDung)
+    // {
+    //     $this->db->query("update nguoi_dung set TenNguoiDung='".$tenNguoiDung."',MatKhau='".$matKhau."', LoaiNguoiDung='".$loaiNguoiDung."' where MaNguoiDung=".$maNguoiDung."");
+    // }
+
+}
+?>
